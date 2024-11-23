a8dfd59d6ae96edc58fad47ad649736d2f2c2031
said.tsamaraev <i.developerpy@yandex.ru>
2024-11-24 00:37:14 +0300

    .

--- keyboards.py
+++ keyboards.py
@@ -2,16 +2,16 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database import SessionLocal, Groups
 
 
-def
-db = SessionLocal()
-try:
-    groups = db.query(Groups).all()
-    for group in groups:
-        print(group.group_name)
-except Exception as e:
-    print(e)
-finally:
-    db.close()
+# def
+# db = SessionLocal()
+# try:
+#     groups = db.query(Groups).all()
+#     for group in groups:
+#         print(group.group_name)
+# except Exception as e:
+#     print(e)
+# finally:
+#     db.close()
 
 
 inline_kb = InlineKeyboardMarkup(inline_keyboard=[
